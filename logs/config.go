f51e3fff85fc7ee480cbfe1b49cb36683375a3ac
Ivan Korobkov <ivan.korobkov@gmail.com>
2017-06-30 10:15:46 +0300

    logs: Fixed DefaultFormat log name.

--- logs/config.go
+++ logs/config.go
@@ -1,7 +1,7 @@
 package logs
 
 const (
-	DefaultFormat     = "${Level}\t${Write}\t${Message}\t${Context}"
+	DefaultFormat     = "${Level}\t${Log}\t${Message}\t${Context}"
 	DefaultTimeFormat = "2006-01-02 15:04:05"
 )
 
