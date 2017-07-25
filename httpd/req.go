ca1407be3471ea42a809be30f7eda9c9a9d165c9
Ivan Korobkov <ivan.korobkov@gmail.com>
2017-07-25 14:06:49 +0300

    httpd: Form int values.

--- httpd/req.go
+++ httpd/req.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-	"net/url"
+	"strconv"
 )
 
 type Req struct {
@@ -21,16 +21,38 @@ func newReq(r *Router, r0 *http.Request, params Params) *Req {
 	}
 }
 
-func (r *Req) Int(param string) int      { return r.Params.Int(param) }
-func (r *Req) Int32(param string) int32  { return r.Params.Int32(param) }
-func (r *Req) Int64(param string) int64  { return r.Params.Int64(param) }
-func (r *Req) Param(param string) string { return r.Params[param] }
+func (r *Req) Int(param string) int {
+	return r.Params.Int(param)
+}
 
-// Form parses and returns the URL query parameters and the POST data.
-// It executes `r.ParseForm(); return r.Request.Form`.
-func (r *Req) Form() url.Values {
-	r.ParseForm()
-	return r.Request.Form
+func (r *Req) Int32(param string) int32 {
+	return r.Params.Int32(param)
+}
+
+func (r *Req) Int64(param string) int64 {
+	return r.Params.Int64(param)
+}
+
+func (r *Req) Param(param string) string {
+	return r.Params[param]
+}
+
+func (r *Req) FormInt(key string) int {
+	v := r.FormValue(key)
+	i, _ := strconv.ParseInt(v, 10, 64)
+	return int(i)
+}
+
+func (r *Req) FormInt32(key string) int32 {
+	v := r.FormValue(key)
+	i, _ := strconv.ParseInt(v, 10, 32)
+	return int32(i)
+}
+
+func (r *Req) FormInt64(key string) int64 {
+	v := r.FormValue(key)
+	i, _ := strconv.ParseInt(v, 10, 64)
+	return i
 }
 
 // DecodeJSON decodes a JSON body into a given destination.
